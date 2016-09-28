7ef2d8ee8a7bfea891a436a27d5f7a546c1d1c78
Tomasz Masternak <tomasz.masternak@particular.net>
2016-09-28 14:48:17 +0200

    Message duplicating behavior

---
+++ src/NServiceBus.ChaosMonkey/NServiceBus.ChaosMonkey.Demo/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace NServiceBus.ChaosMonkey.Demo
+{
+    using System;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync()
+        {
+            var configuration = new EndpointConfiguration("ChaosMonkey.Demo");
+            configuration.UsePersistence<InMemoryPersistence>();
+            configuration.UseTransport<MsmqTransport>();
+
+            configuration.EnableFeature<ChaosMonkey>();
+
+            var endpoint = await Endpoint.Start(configuration);
+
+            await endpoint.SendLocal(new PlaceOrder());
+
+            Console.WriteLine("Press any <key> to close ...");
+            Console.ReadLine();
+        }
+
+        class OrderHandler : IHandleMessages<PlaceOrder>
+        {
+            public Task Handle(PlaceOrder message, IMessageHandlerContext context)
+            {
+                Console.WriteLine("PlaceOrder received.");
+
+                return Task.FromResult(0);
+            }
+        }
+
+        class PlaceOrder : ICommand
+        {
+
+        }
+    }
+}
---
+++ src/NServiceBus.ChaosMonkey/NServiceBus.ChaosMonkey/ChaosMonkey.cs
@@ -0,0 +1,71 @@
+﻿namespace NServiceBus.ChaosMonkey
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Extensibility;
+    using Features;
+    using Pipeline;
+    using Routing;
+    using Transport;
+
+    public class ChaosMonkey : Feature
+    {
+        protected override void Setup(FeatureConfigurationContext context)
+        {
+            context.Pipeline.Register(
+                b => new ChaosBehavior(b.Build<IDispatchMessages>(), context.Settings.LocalAddress()),
+                "Simulates message processing anomalies.");
+        }
+
+        class ChaosBehavior : Behavior<ITransportReceiveContext>
+        {
+            public ChaosBehavior(IDispatchMessages messageDispatcher, string localAddress)
+            {
+                this.messageDispatcher = messageDispatcher;
+                this.localAddress = localAddress;
+            }
+
+            public override async Task Invoke(ITransportReceiveContext context, Func<Task> next)
+            {
+                Console.WriteLine("Message seen in behavior");
+
+                if (context.Message.Headers.ContainsKey(ChaosMonkeyHeader) == false)
+                {
+                    var clone = CreateClone(context.Message);
+
+                    await SendLocal(clone);
+                }
+
+                await next().ConfigureAwait(false);
+            }
+
+            async Task SendLocal(OutgoingMessage message)
+            {
+                var operations = new TransportOperations(new TransportOperation(
+                    message,
+                    new UnicastAddressTag(localAddress),
+                    DispatchConsistency.Isolated));
+
+                await messageDispatcher.Dispatch(operations, new TransportTransaction(), new ContextBag());
+            }
+
+            OutgoingMessage CreateClone(IncomingMessage incomingMessage)
+            {
+                var physicalId = Guid.NewGuid().ToString();
+
+                var headers = incomingMessage.Headers.ToDictionary(kv => kv.Key, kv => kv.Value);
+                headers.Add(ChaosMonkeyHeader, string.Empty);
+
+                var outgoingMessage = new OutgoingMessage(physicalId, headers, incomingMessage.Body);
+
+                return outgoingMessage;
+            }
+
+            string localAddress;
+            IDispatchMessages messageDispatcher;
+
+            const string ChaosMonkeyHeader = "ChaosMonkey";
+        }
+    }
+}
